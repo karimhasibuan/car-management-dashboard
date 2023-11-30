47232868831b941b8a0bbb7d7ec45902b7696197
Karim Hasibuan <amfibicoding19@gmail.com>
2023-12-01 01:26:24 +0700

    feat: use login and profile route.

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,8 +1,11 @@
 import express, { Router } from "express";
 const router: Router = express.Router();
 import upload from "../middleware/upload";
+import authenthicateJWT from "../middleware/authenthicateJWT";
 import * as usersController from "../controllers/usersControllers";
 
 router.post("/register", upload.single("picture"), usersController.register);
+router.post("/login", upload.single("picture"), usersController.login);
+router.get("/profile", authenthicateJWT, usersController.getCurrentUser);
 
 export default router;
