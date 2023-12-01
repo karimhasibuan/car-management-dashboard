0fd72b231758a5c9f21384343ca506732649398a
Karim Hasibuan <amfibicoding19@gmail.com>
2023-12-01 14:44:29 +0700

    feat: add endpoint for superadmin login.

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -6,6 +6,7 @@ import * as usersController from "../controllers/usersControllers";
 
 router.post("/register", upload.single("picture"), usersController.register);
 router.post("/login", upload.single("picture"), usersController.login);
+router.post("/loginsuperadmin", upload.single("picture"), usersController.loginAdmin);
 router.get("/profile", authenthicateJWT, usersController.getCurrentUser);
 
 export default router;
