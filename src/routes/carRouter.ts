80c1e12b6cc16fd1f8f88bb69367631879efafa8
Karim Hasibuan <amfibicoding19@gmail.com>
2023-12-01 16:39:44 +0700

    fix: implement the auth middleware.

--- src/routes/carRouter.ts
+++ src/routes/carRouter.ts
@@ -1,12 +1,14 @@
 import express, { Router } from "express";
 const router: Router = express.Router();
 import upload from "../middleware/upload";
+import authorize from "../middleware/authorize";
+import authenthicateJWT from "../middleware/authenthicateJWT";
 import * as carController from "./../controllers/carsControllers";
 
 router.get("/", carController.get);
 router.get("/:id", carController.getById);
-router.post("/add", upload.single("picture"), carController.post); // add middleware upload.single("picture")
-router.put("/:id", upload.single("picture"), carController.put);
-router.delete("/:id", carController.remove);
+router.post("/add", authenthicateJWT, authorize(["superadmin", "admin"]), upload.single("picture"), carController.post); // add middleware upload.single("picture")
+router.put("/:id", authenthicateJWT, authorize(["superadmin", "admin"]), upload.single("picture"), carController.put);
+router.put("/delete/:id", authenthicateJWT, authorize(["superadmin", "admin"]), carController.remove); // this route for soft delete
 
 export default router;
