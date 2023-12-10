09380bc25e950e2069e4f6ab9ede27ecf4366dfc
Karim Hasibuan <amfibicoding19@gmail.com>
2023-12-10 23:29:08 +0700

    feat: use global styling.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import "./App.css";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import LandingPage from "./components/LandingPage";
 
 const App: React.FC = () => {
