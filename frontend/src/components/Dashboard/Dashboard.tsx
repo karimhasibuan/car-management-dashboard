de571f1d8164af5193e82bdfb5bea38763dbbc54
Karim Hasibuan <amfibicoding19@gmail.com>
2023-12-23 22:21:27 +0700

    issue: can't show the data to the browser. but data can show on console.

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const API = "http://localhost:3001/v1/users/profile";
+
+interface UserProfile {
+  user: Array<{
+    name: string;
+    email: string;
+  }>;
+}
 
 const Dashboard: React.FC = () => {
+  const [userProfile, setUserProfile] = useState<UserProfile>({ user: [] });
+
+  useEffect(() => {
+    if (!userProfile.user.length) {
+      fetch(API, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => setUserProfile(data));
+      console.log(userProfile);
+    }
+  }, [userProfile]);
+
   const handleLogout = () => {
     localStorage.removeItem("token");
     window.location.href = "/login";
@@ -9,7 +34,15 @@ const Dashboard: React.FC = () => {
   return (
     <div>
       <h1>Dashboard</h1>
-      <p>Selamat datang di dashboard!</p>
+      {userProfile.user?.length > 0 &&
+        userProfile.user?.map(({ name, email }) => {
+          return (
+            <div>
+              <p>Selamat datang di dashboard, {name}!</p>
+              <p>Email: {email}</p>
+            </div>
+          );
+        })}
 
       <button onClick={handleLogout}>LOGOUT</button>
     </div>
